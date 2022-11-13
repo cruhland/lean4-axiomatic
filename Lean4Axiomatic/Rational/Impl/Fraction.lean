ffc1524c73229679b5b9f02af222367eabdbcac0
Charles Ruhland <cruhland@alumni.cmu.edu>
2022-11-12 16:28:53 -0800

    Reciprocal on fractions is substitutive

--- Lean4Axiomatic/Rational/Impl/Fraction.lean
+++ Lean4Axiomatic/Rational/Impl/Fraction.lean
@@ -776,6 +776,33 @@ def reciprocal (p : Fraction ℤ) [Nonzero p] : Fraction ℤ := by
 
 postfix:120 "⁻¹" => reciprocal
 
+/--
+The reciprocal operation preserves equivalence.
+
+**Property intuition**: This must be true for reciprocal to be a valid function
+on fractions.
+
+**Proof intuition**: Expand all definitions in the hypotheses and goal until
+equivalences involving only integers are reached. Combine these with algebra to
+finish the proof.
+-/
+theorem recip_subst
+    {p₁ p₂ : Fraction ℤ} [Nonzero p₁] [Nonzero p₂] : p₁ ≃ p₂ → p₁⁻¹ ≃ p₂⁻¹
+    := by
+  revert p₁; intro (a//b); revert p₂; intro (c//d)
+  intro _ _ (_ : a//b ≃ c//d)
+  show (a//b)⁻¹ ≃ (c//d)⁻¹
+  have : Integer.Nonzero a := ‹Nonzero (a//b)›.numerator_nonzero
+  have : Integer.Nonzero c := ‹Nonzero (c//d)›.numerator_nonzero
+  show b//a ≃ d//c
+  show b * c ≃ d * a
+  have : a * d ≃ c * b := ‹a//b ≃ c//d›
+  calc
+    b * c ≃ _ := AA.comm
+    c * b ≃ _ := Rel.symm ‹a * d ≃ c * b›
+    a * d ≃ _ := AA.comm
+    d * a ≃ _ := Rel.refl
+
 instance rational : Rational (Fraction ℤ) := {
   eqvOp := eqvOp
   addOp := addOp
